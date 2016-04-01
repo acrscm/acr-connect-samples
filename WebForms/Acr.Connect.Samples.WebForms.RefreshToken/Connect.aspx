76130f2f54549b534c4084d5560faa0a38ba4a81
Sergei Rusinov <rsage@mail.ru>
2016-04-01 11:21:35 -0400

    Access and Refresh token retrieving was added to WebForm Refresh token sample.

---
+++ WebForms/Acr.Connect.Samples.WebForms.RefreshToken/Connect.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="Connect to ACR" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Connect.aspx.cs" Inherits="Acr.Connect.Samples.WebForms.RefreshToken.Connect" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    
+    <table>
+        <tr>
+            <td>Authentication Service URL:</td>
+            <td><asp:TextBox ID="AuthenticationServiceUrl" Width="500" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Client ID:</td>
+            <td><asp:TextBox ID="ClientId" Width="500" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Client Secret:</td>
+            <td><asp:TextBox ID="ClientSecret" Width="500" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Redirect URL:</td>
+            <td><asp:TextBox ID="RedirectUrl" Width="500" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Scope:</td>
+            <td><asp:TextBox ID="Scope" Width="500" runat="server"></asp:TextBox></td>
+        </tr>
+    </table>
+    <br>
+
+    <h3>Click here to verify your ACR account.</h3>
+    <asp:Button Text="Login to ACR" runat="server" OnClick="OnClick"/>
+    <br>
+    <asp:Label runat="server" ForeColor="Red">Refresh Token:</asp:Label>
+    <br>
+    <asp:Label ID="RefreshToken" runat="server"></asp:Label>
+    <br>
+    <asp:Button ID="GetAccessTokenButton" Text="Request Access Token" runat="server" OnClick="GetAccessTokenButton_OnClick" Enabled="False"/>
+    <br>
+    <asp:Label runat="server" ForeColor="Red">Access Token:</asp:Label>
+    <br>
+    <asp:Label ID="AccessToken" runat="server"></asp:Label>
+    <br>
+</asp:Content>
---
+++ WebForms/Acr.Connect.Samples.WebForms.RefreshToken/Connect.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Web;
+using Acr.Connect.Samples.WebForms.RefreshToken.Properties;
+using Acr.Connect.Security.Common;
+using Microsoft.IdentityModel.Protocols;
+using Thinktecture.IdentityModel.Client;
+
+namespace Acr.Connect.Samples.WebForms.RefreshToken
+{
+    public partial class Connect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                var settigs = Settings.Default;
+                AuthenticationServiceUrl.Text = settigs.AuthenticationServiceUrl;
+                ClientId.Text = settigs.ClientId;
+                ClientSecret.Text = settigs.ClientSecret;
+                RedirectUrl.Text = settigs.RedirectUrl;
+                Scope.Text = settigs.Scope;
+            }
+
+            var oidcMessage = new OpenIdConnectMessage(HttpUtility.ParseQueryString(Request.Url.Query));
+            if (string.IsNullOrEmpty(oidcMessage.Code)) return;
+
+            RequestsTokensByCode(oidcMessage.Code);
+        }
+
+        protected void OnClick(object sender, EventArgs e)
+        {
+            RedirectToAuthenticationEndpont();
+        }
+
+        protected void GetAccessTokenButton_OnClick(object sender, EventArgs e)
+        {
+            RequestTokensByRefreshToken(RefreshToken.Text);
+        }
+
+        private void RedirectToAuthenticationEndpont()
+        {
+            using (var acrIdSingInOptions = new OidcOptions())
+            {
+                acrIdSingInOptions.AuthenticationServiceBaseUrl = new Uri(AuthenticationServiceUrl.Text, UriKind.Absolute);
+                acrIdSingInOptions.ClientId = ClientId.Text;
+                acrIdSingInOptions.RedirectUri = new Uri(RedirectUrl.Text, UriKind.Absolute);
+                acrIdSingInOptions.Scope = Scope.Text;
+
+                Response.Redirect(acrIdSingInOptions.SignInUrl.OriginalString);
+            }
+        }
+
+        private void RequestsTokensByCode(string authCode)
+        {
+            using (var acrIdSingInOptions = new OidcOptions())
+            {
+                acrIdSingInOptions.AuthenticationServiceBaseUrl = new Uri(AuthenticationServiceUrl.Text, UriKind.Absolute);
+
+                var client = new OAuth2Client(acrIdSingInOptions.TokenEndpointUrl, ClientId.Text, ClientSecret.Text,
+                    OAuth2Client.ClientAuthenticationStyle.PostValues);
+                var tokenResponse = client.RequestAuthorizationCodeAsync(authCode, RedirectUrl.Text).Result;
+
+                RefreshToken.Text = tokenResponse.RefreshToken;
+                GetAccessTokenButton.Enabled = !string.IsNullOrEmpty(tokenResponse.RefreshToken);
+            }
+        }
+
+        private void RequestTokensByRefreshToken(string refreshToken)
+        {
+            using (var acrIdSingInOptions = new OidcOptions())
+            {
+                acrIdSingInOptions.AuthenticationServiceBaseUrl = new Uri(AuthenticationServiceUrl.Text, UriKind.Absolute);
+
+                var client = new OAuth2Client(acrIdSingInOptions.TokenEndpointUrl, ClientId.Text, ClientSecret.Text,
+                    OAuth2Client.ClientAuthenticationStyle.PostValues);
+                var tokenResponse = client.RequestRefreshTokenAsync(refreshToken).Result;
+
+                RefreshToken.Text = tokenResponse.RefreshToken;
+                AccessToken.Text = tokenResponse.AccessToken;
+            }
+        }
+    }
+}
