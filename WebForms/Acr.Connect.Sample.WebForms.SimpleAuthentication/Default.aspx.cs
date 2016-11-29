6dedc9b68669f8d77f582db599f92353fbd76812
Sergei Rusinov <srusinov@acr.org>
2016-11-29 15:30:55 -0500

    Web Forms Sample: Simple authentication sample was implemented.

---
+++ WebForms/Acr.Connect.Sample.WebForms.SimpleAuthentication/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace Acr.Connect.Sample.WebForms.SimpleAuthentication
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Context.GetOwinContext().Authentication.User.Identity.IsAuthenticated)
+            {
+                LoginButton.Visible = false;
+                Label.Text = Context.GetOwinContext().Authentication.User.Identity.Name;
+                return;
+            }
+
+            var loginInfo = Context.GetOwinContext().Authentication.GetExternalLoginInfo();
+            if (loginInfo != null)
+            {
+                var user = ConvertToAppUser(loginInfo);
+                Context.GetOwinContext().Authentication.SignIn(user);
+                Response.Redirect("~/");
+            }
+
+            LoginButton.Visible = true;
+        }
+
+        private static ClaimsIdentity ConvertToAppUser(ExternalLoginInfo loginInfo)
+        {
+            var user = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, loginInfo.Login.ProviderKey),
+                new Claim(ClaimTypes.Name, loginInfo.Login.ProviderKey)
+            }, DefaultAuthenticationTypes.ApplicationCookie);
+
+            return user;
+        }
+
+        protected void LoginButton_OnClick(object sender, EventArgs e)
+        {
+            var properties = new AuthenticationProperties { RedirectUri = ResolveUrl("~/") };
+            Context.GetOwinContext().Authentication.Challenge(properties, "ACR ID");
+            Response.StatusCode = 401;
+            Response.End();
+        }
+    }
+}
---
+++ WebForms/Acr.Connect.Sample.WebForms.SimpleAuthentication/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using Acr.Connect.Owin.Security.Oidc;
+using Acr.Connect.Sample.WebForms.SimpleAuthentication;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Acr.Connect.Sample.WebForms.SimpleAuthentication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAcrConnect(app);
+            ConfigureCookieAuth(app);
+        }
+
+        private static void ConfigureAcrConnect(IAppBuilder app)
+        {
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            var certificate = HttpContext.Current.Server.MapPath("~/AcrConnect.cer");
+            var cert = new X509Certificate2(certificate);
+
+            var saOptions = new AcrConnectOidcAuthenticationOptions
+            {
+                IssuerName = "https://acr-id-test.acr.org/SecureAuth17",
+                CallbackPath = new PathString("/account/accesstoken"),
+                ClientId = "c3b97253202a426390cccfac755dc80c",
+                ClientSecret = "a596fb52d41749b092cfcda57cf6197b",
+                RealmUrl = new Uri("https://acr-id-test.acr.org/acrid-userlogin/"),
+                PostLogOffRedirectPath = new PathString("/Default.aspx"),
+                IssuerSigningKey = new X509AsymmetricSecurityKey(cert)
+            };
+
+            app.UseAcrConnectOidcAuthentication(saOptions);
+        }
+
+        private static void ConfigureCookieAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Index"),
+                SlidingExpiration = true,
+                ExpireTimeSpan = TimeSpan.FromMinutes(2)
+            });
+        }
+    }
+}
