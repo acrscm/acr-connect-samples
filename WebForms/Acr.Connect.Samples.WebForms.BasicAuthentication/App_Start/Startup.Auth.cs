a09a4ae3ee1caa4c9e8263c6c089f3232d1cd23d
Sergei Rusinov <srusinov@acr.org>
2016-11-29 13:28:51 -0500

    Web Forms Sample: ACR Connect based authentication was enabled.

--- WebForms/Acr.Connect.Samples.WebForms.BasicAuthentication/App_Start/Startup.Auth.cs
+++ WebForms/Acr.Connect.Samples.WebForms.BasicAuthentication/App_Start/Startup.Auth.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.IdentityModel.Tokens;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using Acr.Connect.Owin.Security.Oidc;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.DataProtection;
-using Microsoft.Owin.Security.Google;
 using Owin;
 using Acr.Connect.Samples.WebForms.BasicAuthentication.Models;
 
@@ -13,57 +13,47 @@ namespace Acr.Connect.Samples.WebForms.BasicAuthentication
 {
     public partial class Startup {
 
-        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301883
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
-            // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
+            ConfigureAcrConnect(app);
+
+            ConfigureCookieAuth(app);
+        }
+
+        private static void ConfigureAcrConnect(IAppBuilder app)
+        {
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            var certificate = HttpContext.Current.Server.MapPath("~/AcrConnect.cer");
+            var cert = new X509Certificate2(certificate);
+
+            var saOptions = new AcrConnectOidcAuthenticationOptions
+            {
+                IssuerName = "https://acr-id-test.acr.org/SecureAuth17",
+                CallbackPath = new PathString("/account/accesstoken"),
+                ClientId = "c3b97253202a426390cccfac755dc80c",
+                ClientSecret = "a596fb52d41749b092cfcda57cf6197b",
+                RealmUrl = new Uri("https://acr-id-test.acr.org/acrid-userlogin/"),
+                PostLogOffRedirectPath = new PathString("/Default.aspx"),
+                IssuerSigningKey = new X509AsymmetricSecurityKey(cert)
+            };
+
+            app.UseAcrConnectOidcAuthentication(saOptions);
+        }
+
+        private static void ConfigureCookieAuth(IAppBuilder app)
+        {
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
+                LoginPath = new PathString("/Home/Index"),
+                SlidingExpiration = true,
+                ExpireTimeSpan = TimeSpan.FromMinutes(2)
             });
-            // Use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
-
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
-
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
-
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
-
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
         }
     }
 }
