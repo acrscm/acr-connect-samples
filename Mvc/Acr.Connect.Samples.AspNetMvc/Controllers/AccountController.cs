b2936817ef40dd5106f6b64f31f32b878413919d
srusinov <srusinov@acr.org>
2019-06-04 10:21:37 -0400

    MVC Sample: login with OIDC.

---
+++ Mvc/Acr.Connect.Samples.AspNetMvc/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Acr.Connect.Samples.AspNetMvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAuthenticationManager AuthManager => HttpContext.GetOwinContext().Authentication;
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login()
+        {
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("Index", "Home") };
+            AuthManager.Challenge(properties, "acrid");
+
+            return new HttpUnauthorizedResult();
+        }
+
+        [HttpPost]
+        //[AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logoff()
+        {
+            AuthManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Mvc/Acr.Connect.Samples.AspNetMvc/Startup.cs
@@ -0,0 +1,89 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNet.Identity;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+
+[assembly: OwinStartup(typeof(Acr.Connect.Samples.AspNetMvc.Startup))]
+
+namespace Acr.Connect.Samples.AspNetMvc
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+
+            var oidcOptions = new OpenIdConnectAuthenticationOptions()
+            {
+                SignInAsAuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                AuthenticationType = "acrid",
+                Authority = "https://acrsr.oktapreview.com",
+                ClientId = "0oa1hm4POxgJM6CPu0g4",
+                ClientSecret = "qweqweqweqweqweq",
+                ResponseType = "code",
+                RedirectUri = "http://localhost:62529/oidc-callback",
+                Scope = "openid email profile"
+            };
+            oidcOptions.Notifications = new OpenIdConnectAuthenticationNotifications
+            {
+                MessageReceived = MessageReceived,
+                SecurityTokenValidated = SecurityTokenValidated
+            };
+
+            app.UseOpenIdConnectAuthentication(oidcOptions);
+        }
+
+        private async Task MessageReceived(MessageReceivedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> arg)
+        {
+            var httpClient = new HttpClient();
+            var config = await arg.Options.ConfigurationManager.GetConfigurationAsync(CancellationToken.None);
+            var request = new AuthorizationCodeTokenRequest
+            {
+                Code = arg.ProtocolMessage.Code,
+                ClientId = arg.Options.ClientId,
+                ClientSecret = arg.Options.ClientSecret,
+                RedirectUri = arg.Options.RedirectUri,
+                Address = config.TokenEndpoint
+            };
+            var response = await httpClient.RequestAuthorizationCodeTokenAsync(request);
+
+            arg.ProtocolMessage.IdToken = response.IdentityToken;
+            arg.ProtocolMessage.AccessToken = response.AccessToken;
+            arg.ProtocolMessage.Code = null;
+        }
+
+        private async Task SecurityTokenValidated(SecurityTokenValidatedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> arg)
+        {
+            var httpClient = new HttpClient();
+            var config = await arg.Options.ConfigurationManager.GetConfigurationAsync(CancellationToken.None);
+            var request = new UserInfoRequest
+            {
+                Address = config.UserInfoEndpoint,
+                Token = arg.ProtocolMessage.AccessToken
+            };
+            var response = await httpClient.GetUserInfoAsync(request);
+
+            arg.AuthenticationTicket.Identity.AddClaim(response.Claims.First(c => c.Type == "email_verified"));
+
+            var nameClaim = response.Claims.First(c => c.Type == "name");
+
+            arg.AuthenticationTicket.Identity.AddClaim(new Claim(ClaimTypes.Name, nameClaim.Value));
+        }
+    }
+}
